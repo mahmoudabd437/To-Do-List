035eea6f0336cfbec70f2834f22d8bfe2731b8bf
mahmoudabd437 <141342718+mahmoudabd437@users.noreply.github.com>
2024-05-10 18:57:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+let input = document.querySelector('.input');
+let submit = document.querySelector('.add');
+let divTasks = document.querySelector('.tasks');
+
+let arrTasks = [];
+
+// check localStorage empty or Not
+if (localStorage.getItem('task') !== null) {
+    arrTasks = JSON.parse(localStorage.getItem('task'));
+} else {
+    arrTasks = [];
+}
+// add tasks in div
+function addTasks() {
+    let taskValue = '';
+    for (let i = 0; i < arrTasks.length; i++) {
+        taskValue += `<p class="task" onclick="doneTask(${i})">${arrTasks[i]}
+            <span class="delete"
+                    onclick="deleteTask(${i})">delete</span></p>`;
+    }
+    divTasks.innerHTML = taskValue;
+}
+addTasks();
+
+// add tasks in div when click on submit
+
+submit.onclick = function () {
+
+    if (input.value !== "") {
+        arrTasks.push(input.value);
+        localStorage.setItem('task', JSON.stringify(arrTasks));
+        addTasks();
+    }
+    input.value = '';
+};
+
+// delete tasks from div
+
+function deleteTask(i) {
+    arrTasks.splice(i, 1);
+    localStorage.setItem('task', JSON.stringify(arrTasks));
+    addTasks();
+};
+
+// add class done when click on done
+
+function doneTask(i) {
+    let task = document.querySelectorAll('.task');
+    task[i].classList.toggle('done');
+};
